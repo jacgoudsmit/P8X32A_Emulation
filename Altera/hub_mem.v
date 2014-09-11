c4a053eeb96732983354e2b340482de96f38d128
Jac Goudsmit <jac_goudsmit@yahoo.com>
2014-09-10 22:55:45 -0700

    Changing code to use $readmemh for ROM initialization
    Also adding ROM images for each separate ROM area, though currently only two of those files are being used.
    The new image files were generated from the previous build, using Cluso99's ROM dump Spin program.

--- Altera/hub_mem.v
+++ Altera/hub_mem.v
@@ -91,9 +91,14 @@ endgenerate
 
 // 4096 x 32 rom containing character definitions ($8000..$BFFF)
 
-(* ram_init_file = "hub_rom_low.hex" *)     reg [31:0] rom_low [4095:0];
+reg [31:0] rom_low [4095:0];
 
 reg [31:0] rom_low_q;
+
+initial
+begin
+    $readmemh("rom_8000_bfff_font.hex", rom_low);
+end
 
 always @(posedge clk_cog)
 if (ena_bus && a[13:12] == 2'b10)
@@ -102,9 +107,14 @@ if (ena_bus && a[13:12] == 2'b10)
 
 // 4096 x 32 rom containing sin table, log table, booter, and interpreter ($C000..$FFFF)
 
-(* ram_init_file = "hub_rom_high.hex" *)    reg [31:0] rom_high [4095:0];
+reg [31:0] rom_high [4095:0];
 
 reg [31:0] rom_high_q;
+
+initial
+begin
+    $readmemh("rom_c000_ffff_scrambled.hex", rom_high);
+end
 
 always @(posedge clk_cog)
 if (ena_bus && a[13:12] == 2'b11)
