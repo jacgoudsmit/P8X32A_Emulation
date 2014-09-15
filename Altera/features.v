37966446c22cb5f8cc611c7c39d074f0cb348dce
Jac Goudsmit <jac_goudsmit@yahoo.com>
2014-09-15 00:16:58 -0700

    Finished rewriting the hub_mem module
    The HUB ROM and RAM sizes are now specified in bytes, and can be overridden from the project settings by using a macro.
    Also the ROM can be left unscrambled with a macro.

--- Altera/features.v
+++ Altera/features.v
@@ -30,3 +30,65 @@ the Propeller 1 Design.  If not, see <http://www.gnu.org/licenses/>.
 `ifdef INVERT_COG_LEDS
 defparam core.INVERT_COG_LEDS = `INVERT_COG_LEDS;
 `endif
+
+
+//-----------------------------------------------------------------------------
+// Unscrambled ROM
+//
+// If this is defined, the unscrambled version of the interpreter and boot
+// loader are used, instead of the usual scrambled version as found on the
+// Propeller chip.
+// The Propeller chip has some circuitry that swaps the bits of some areas
+// of the ROM if you try to read them directly from a program, because when
+// the chip was originally designed, there was some concern that others might
+// make counterfit Propellers. Later on, Parallax decided to open-source the
+// original source code for the binary code that's on-board the chip.
+// The scrambling circuitry is no longer necessary, and it saves some space if
+// the chip doesn't have it, but strictly speaking, it makes the ROM content
+// different from the original, so this option is off by default.
+//
+//`define ENABLE_UNSCRAMBLED_ROM
+
+
+//-----------------------------------------------------------------------------
+// Hub ROM size
+//
+// Define this to override the ROM size used. Not all hardware can support the
+// entire ROM. For example, on the DE0-Nano, the font doesn't fit so its .qsf
+// file has an override for this macro.
+// The default ROM size is 32768 bytes, which is also the maximum.
+// The minimum ROM size is 4092 bytes, otherwise the system won't boot.
+// Depending on the ROM size, the following features will be available:
+//
+// Required     ROM starts  Data
+// ROM Size:    at Address:
+// ------------------------------------
+// 32768        $8000       Font
+// 16384        $C000       Logarithmic table
+// 12288        $D000       Anti-logarithmic table
+// 8192         $E000       Sine table
+// 4092         $F004       Spin interpreter
+// (*)          $F800       Boot loader
+// (*)          $FF00       Copyright message
+// (*)          $FF70       Runner
+//
+// (*) Do not set the ROM size to a value below the minimum, or the system
+// won't boot. The interpreter, boot loader and runner are all needed.
+`ifdef HUB_ROM_SIZE_BYTES
+defparam core.hub_.hub_mem_.HUB_ROM_SIZE_BYTES = `HUB_ROM_SIZE_BYTES;
+`endif
+
+
+//-----------------------------------------------------------------------------
+// Hub RAM size
+//
+// Define this to change the size of the hub RAM. The default size is 32768.
+// Note: when the chip loads a program from EEPROM or serial port, it still
+// loads 32KB regardless of this setting.
+// If you set the RAM to be larger, you can store more data but you won't be
+// able to access the ROM that would normally be in that place.
+// If you set the RAM size to more than 60KB, the system won't boot because
+// the top 4KB of the hub are needed to start up.
+`ifdef HUB_RAM_SIZE_BYTES
+defparam core.hub_.hub_mem_.HUB_RAM_SIZE_BYTES = `HUB_RAM_SIZE_BYTES;
+`endif
--- Altera/hub_mem.v
+++ Altera/hub_mem.v
@@ -22,12 +22,13 @@ the Propeller 1 Design.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-// Submodule for one 8-bit block of RAM
+// Hub RAM
+//
 // This is instantiated 4 times to allow writing by byte, word or long
 
-module              byte_ram
+module              hub_ram
 (
-input               clk_cog,
+input               clk,
 input               enable,
 input               w,
 input               wb,
@@ -37,23 +38,90 @@ input        [7:0]  d8,
 output       [7:0]  q8
 );
 
-parameter HUB_RAM_SIZE_LONGS = 8192;
+// Size of RAM block in bytes.
+parameter SIZE_BYTES;
 
-reg [7:0] ram[HUB_RAM_SIZE_LONGS - 1:0];
+reg [7:0] ram[SIZE_BYTES - 1:0];
 
-always @(posedge clk_cog)
+always @(posedge clk)
 begin
-    if (enable)
+    if (enable && w && wb)
     begin
-        if (w && wb)
-            ram[a] <= d8;
-        q8 <= ram[a[12:0]];
+        ram[a] <= d8;
     end
+    q8 <= ram[a];
 end
 
 endmodule
 
 
+//-----------------------------------------------------------------------------
+// Hub ROM
+//
+// The ROM size can be varied by overriding the parameter, but it's always
+// mapped to the top of the hub space, regardless of whether the entire 32KB
+// ROM is implemented or not.
+// If the ROM size parameter is set to a value less than 8192 (32KB),
+// the missing amount of ROM is mapped in front of the actual ROM.
+// For example, if the ROM size is set to the minimum of 512 (4KB), the
+// actual ROM starts at $F000 instead of $8000, and the macros declared
+// below will insert 28KB of "missing ROM" in addresses $8000-$EFFF.
+// A couple of macros are used to initialize the ROM and calculate addresses
+// based on its size and/or the hub address where the image files need to
+// be stored.
+
+module hub_rom
+(
+input               clk,
+input               enable,
+
+input       [13:0]  a,
+input       [31:0]  d,
+
+output      [31:0]  q
+);
+
+
+// Size of hub ROM in longs
+parameter SIZE_LONGS;
+
+reg [31:0] rom [SIZE_LONGS - 1:0];
+
+// Amount of missing ROM in longs
+`define ROMMISSING (8192 - SIZE_LONGS)
+// Calculate offset in ROM area ($8000-$FFFF) based on no missing ROM
+`define ROMOFFSET(hubaddress) (((hubaddress) - 'h8000) >> 2)
+// Init ROM from file unless the destination ROM is missing
+`define ROMINIT(file, hubstart, hubend) \
+    if (`ROMOFFSET(hubstart) >= `ROMMISSING) \
+        $readmemh(file, rom, `ROMOFFSET(hubstart) - `ROMMISSING, `ROMOFFSET(hubend) - `ROMMISSING)
+
+initial
+begin
+    `ROMINIT("rom_8000_bfff_font.hex",                      'h8000, 'hBFFF);
+    `ROMINIT("rom_c000_cfff_log.hex",                       'hC000, 'hCFFF);
+    `ROMINIT("rom_d000_dfff_antilog.hex",                   'hD000, 'hDFFF);
+    `ROMINIT("rom_e000_f003_sine.hex",                      'hE000, 'hF003);
+`ifdef ENABLE_UNSCRAMBLED_ROM
+    `ROMINIT("rom_f004_f7a3_unscrambled_interpreter.hex",   'hF004, 'hF7A3);
+    `ROMINIT("rom_f800_fb93_unscrambled_booter.hex",        'hF800, 'hFB93);
+`else
+    `ROMINIT("rom_f004_f7a3_scrambled_interpreter.hex",     'hF004, 'hF7A3);
+    `ROMINIT("rom_f800_fb93_scrambled_booter.hex",          'hF800, 'hFB93);
+`endif
+    `ROMINIT("rom_ff00_ff5f_copyright.hex",                 'hFF00, 'hFF5F);
+    `ROMINIT("rom_ff70_ffff_runner.hex",                    'hFF70, 'hFFFF);
+end
+
+always @(posedge clk)
+begin
+    q <= rom[a - `ROMMISSING];
+end
+
+endmodule
+
+
+//-----------------------------------------------------------------------------
 // Hub memory
 
 module              hub_mem
@@ -71,68 +139,70 @@ output      [31:0]  q
 );
 
 
+// RAM and ROM sizes in blocks.
+// The standard Propeller has 32KB RAM, 32KB ROM
+// RAM size should be 32KB, reduce at your own risk
+// ROM should be between 512 and  8192 longs (4KB..32KB)
+parameter HUB_RAM_SIZE_BYTES = 32768;
+parameter HUB_ROM_SIZE_BYTES = 32768;
+
+// Check if address is in RAM
+// TODO: simplify with log2 combined with logical AND
+wire in_ram = (a < HUB_RAM_SIZE_BYTES >> 2);
+
+// Check if address is in ROM
+// TODO: simplify with log2 combined with logical AND
+wire in_rom = (a >= 16384 - (HUB_ROM_SIZE_BYTES >> 2));
+
+// Instantiate 4 instances of RAM
+
 reg [31:0] ram_q;
 
 genvar i;
 generate
     for (i = 0; i < 4; i = i + 1)
     begin : ramgen
-        byte_ram ram_(  .clk_cog    (clk_cog),
-                        .enable     (ena_bus),
-                        .w          (w),
-                        .wb         (wb[i]),
-                        .a          (a),
-                        .d8         (d[(i+1)*8-1:i*8]),
-                        .q8         (ram_q[(i+1)*8-1:i*8])
-                      );
+        hub_ram 
+            #(
+                    .SIZE_BYTES (HUB_RAM_SIZE_BYTES >> 2)
+            )
+            ram_(
+                    .clk        (clk_cog),
+                    .enable     (ena_bus && in_ram),
+                    .w          (w),
+                    .wb         (wb[i]),
+                    .a          (a),
+                    .d8         (d[(i+1)*8-1:i*8]),
+                    .q8         (ram_q[(i+1)*8-1:i*8])
+            );
     end
 endgenerate
 
 
-// 4096 x 32 rom containing character definitions ($8000..$BFFF)
+// Instantiate ROM
 
-reg [31:0] rom_low [4095:0];
+reg [31:0] rom_q;
 
-reg [31:0] rom_low_q;
-
-initial
-begin
-    $readmemh("rom_8000_bfff_font.hex", rom_low);
-end
-
-always @(posedge clk_cog)
-if (ena_bus && a[13:12] == 2'b10)
-    rom_low_q <= rom_low[a[11:0]];
-
-
-// 4096 x 32 rom containing sin table, log table, booter, and interpreter ($C000..$FFFF)
-
-reg [31:0] rom_high [4095:0];
-
-reg [31:0] rom_high_q;
-
-initial
-begin
-    $readmemh("rom_c000_ffff_scrambled.hex", rom_high);
-end
-
-always @(posedge clk_cog)
-if (ena_bus && a[13:12] == 2'b11)
-    rom_high_q <= rom_high[a[11:0]];
-
+hub_rom 
+    #(
+                    .SIZE_LONGS (HUB_ROM_SIZE_BYTES >> 2)
+    )
+    rom_(
+                    .clk        (clk_cog),
+                    .enable     (ena_bus && in_rom),
+                    .a          (a),
+                    .d          (d),
+                    .q          (rom_q)
+    );
 
 // memory output mux
 
-reg [1:0] mem;
+reg mem;
 
 always @(posedge clk_cog)
 if (ena_bus)
-    mem <= a[13:12];
+    mem <= in_ram;
 
-assign q            = !mem[1]   ? ram_q
-`ifndef DISABLE_CHARACTER_ROM
-                    : !mem[0]   ? rom_low_q
-`endif
-                                : rom_high_q;
+assign q = mem ? ram_q : rom_q;
 
 endmodule
